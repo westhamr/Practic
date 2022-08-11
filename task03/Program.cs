ae66fae162b85a8265899a49eff85c31a7af76f1
westhamr <westhamr@yandex.ru>
2022-08-11 22:49:53 +0700

    add Task 03

---
+++ task03/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Напишите число ");
+int number = int.Parse(Console.ReadLine());
+
+bool check; 
+
+if (number % 2 == 0)
+{
+    check = true;
+}
+else
+{
+    check = false;
+}
+Console.WriteLine(check ? "Число четное" : "Число нечетное");
