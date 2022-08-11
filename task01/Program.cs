5e4a752e26c023f531dc289f32ac9d576ad0e697
westhamr <westhamr@yandex.ru>
2022-08-11 22:07:47 +0700

    add Task 1

---
+++ task01/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Напишите первое число ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Напишите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.Write("Меньшее = "+ number2 + ", Большее = " + number1);   
+}
+if (number2 > number1)
+{
+    Console.Write("Меньшее = "+ number1 + ", Большее = " + number2);   
+}
+if (number1 == number2)
+{
+     Console.Write("Числа равны"); 
+}
