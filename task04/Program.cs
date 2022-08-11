fb87a401cbbba0e085a8e7dd3fd824a03147c4b0
westhamr <westhamr@yandex.ru>
2022-08-11 23:01:12 +0700

    add Task 04

---
+++ task04/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Напишите число ");
+int number = int.Parse(Console.ReadLine());
+
+int count = 1;
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + " ");
+    }
+    count++;
+}
