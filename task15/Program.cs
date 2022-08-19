1a26d46fadc40c848f1ab23c3b44e3790c4902e3
westhamr <westhamr@yandex.ru>
2022-08-19 17:00:28 +0700

    add task15

---
+++ task15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Напишите номер дня недели ");
+int number = int.Parse(Console.ReadLine());
+if(number > 7){
+    Console.Write("Нет столько дней в неделе ");
+    return;
+}
+if(number == 7 || number == 6 ){
+    Console.WriteLine("Выходной");
+}
+else{
+    Console.WriteLine("Будний день");
+}
