16b1daecf6fd548f1fff546b474b3e0944fb9ebd
westhamr <westhamr@yandex.ru>
2022-08-19 16:42:45 +0700

    Add task13

---
+++ task13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Напишите число ");
+int number = int.Parse(Console.ReadLine());
+if(number < 100){
+    Console.WriteLine("Третьей цифры нет");
+}
+else{
+    int num1 = number / 100;
+    int result = num1 % 10;
+    Console.WriteLine(result);
+}
