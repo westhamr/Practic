227e504e044ed10e32801accb17bdade37e02d2f
westhamr <westhamr@yandex.ru>
2022-08-19 15:43:12 +0700

    add task10

---
+++ task10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Напишите трехзначное число ");
+int number = int.Parse(Console.ReadLine());
+string str = number.ToString();
+int[] b = new int[str.Length];
+for( int i=0; i< str.Length; i++)
+{
+     b[i] = int.Parse(str[i].ToString());
+}
+Console.WriteLine(b[1]);
