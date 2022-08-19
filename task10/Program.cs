21ad13f3310b8c3059e56b93a27603fb9f69d3a7
westhamr <westhamr@yandex.ru>
2022-08-19 16:28:32 +0700

    Edit task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,9 +1,5 @@
 ﻿Console.Write("Напишите трехзначное число ");
 int number = int.Parse(Console.ReadLine());
-string str = number.ToString();
-int[] b = new int[str.Length];
-for( int i=0; i< str.Length; i++)
-{
-     b[i] = int.Parse(str[i].ToString());
-}
-Console.WriteLine(b[1]);
+int num23 = number % 100;
+int result = num23 / 10;
+Console.WriteLine(result);
