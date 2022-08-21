d476b6801287ad635ea8ef9a889f9039a27a5c94
westhamr <westhamr@yandex.ru>
2022-08-21 22:20:42 +0700

    add task 27

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿int Sum (int num)
+{
+    int numSum = 0;
+    while (num > 0)
+    {
+        numSum = numSum + (num % 10);
+        num = num / 10;
+    }
+    return numSum;
+}
+
+Console.Write("Напишите число ");
+int number = int.Parse(Console.ReadLine());
+
+int result = Sum(number);
+Console.Write(result);
