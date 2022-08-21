7d82b1ba6031bfb1dfe897e9df520d200b5c8856
westhamr <westhamr@yandex.ru>
2022-08-21 22:34:19 +0700

    add task 29

---
+++ task29/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Напишите длинну массива ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("Напишите начальное число ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Напишите конечное число ");
+int b = int.Parse(Console.ReadLine());
+
+int[] array = new int[N];
+int length = array.Length;
+for (int i = 0; i < length; i++)
+{
+    array[i] = new Random().Next(a,b);
+}
+for (int j = 0; j < length; j++)
+{
+    Console.Write(array[j] + " ");
+}
