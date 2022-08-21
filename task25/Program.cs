7b7e7d24aafc241cced31c969823b08d6364b64f
westhamr <westhamr@yandex.ru>
2022-08-21 22:01:20 +0700

    add task 25

---
+++ task25/Program.cs
@@ -0,0 +1,18 @@
+﻿int Pow (int num, int numX)
+{
+    int result = num;
+    for (int i = 1; i < numX; i++)
+    {
+        result = result * num;
+    }
+    return result;
+}
+
+
+Console.Write("Напишите число ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Напишите число в какую степень возводим ");
+int numberX = int.Parse(Console.ReadLine());
+
+int result =  Pow(number, numberX);
+Console.Write(result);
