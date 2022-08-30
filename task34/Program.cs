45a2e2f831b7ab85dfa034c8c7f8c4d3b701ffb6
westhamr <westhamr@yandex.ru>
2022-08-31 00:20:33 +0700

    Add task 34

---
+++ task34/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int[5];
+int length = array.Length;
+int count = 0;
+for (int i = 0; i < length; i++)
+{
+    array[i] = new Random().Next(100, 999);
+}
+for (int j = 0; j < length; j++)
+{
+    Console.Write(array[j] + " ");
+    if(array[j] % 2 == 0)
+    {
+        count += 1;
+    }
+}
+Console.Write("-> " + count);
