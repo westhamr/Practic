ee26b61a5881dda59d3c43625627962e322c7b12
westhamr <westhamr@yandex.ru>
2022-08-31 00:31:08 +0700

    Add task 36

---
+++ task36/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = new int[5];
+int length = array.Length;
+int sum = 0;
+for (int i = 0; i < length; i++)
+{
+    array[i] = new Random().Next(1, 100);
+}
+for (int j = 0; j < length; j++)
+{
+    Console.Write(array[j] + " ");
+    if(j % 2 != 0)
+    {
+        sum += array[j];
+    }
+}
+Console.Write("Сумма на нечетных позициях: " + sum);
