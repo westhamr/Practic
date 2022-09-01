9c0f19412ee26495c31c587bcd066119d1390a63
westhamr <westhamr@yandex.ru>
2022-09-02 01:07:23 +0700

    Add task 41

---
+++ task41/Program.cs
@@ -0,0 +1,16 @@
+﻿int CountNum(int m)
+{
+    int count = 0;
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введите число {i + 1} ");
+        int num = int.Parse(Console.ReadLine());
+        if(num > 0) count++;
+    }
+    return count;
+}
+
+Console.Write($"Введите количество чисел ");
+int col = int.Parse(Console.ReadLine());
+int c = CountNum(col);
+Console.Write($"Колличество положительных чисел = {+ c} ");
