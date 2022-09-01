c4ec54c5c03b730890a9ddca8d2a1bb8f4ceb70b
westhamr <westhamr@yandex.ru>
2022-09-02 01:55:59 +0700

    add task 43

---
+++ task43/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+ 
+Console.WriteLine($"({x};{y})");
